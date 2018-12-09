5bdec202cc6c309df33ca2abc768162c6bb1cd64
zzzlk123 <9464534+zzzlk123@users.noreply.github.com>
2018-12-09 20:45:20 +0800

    finish basic horse game

--- game_dispatcher.go
+++ game_dispatcher.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"nbot/games"
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/zzzlk123/nbot/games"
 )
 
+var h = horse.Game{}
+
 func main() {
-	g := horse.Game{}
-	fmt.Println(g)
-	g.StartGame()
-	fmt.Println(g)
-	fmt.Println(g.GetGameDescribe())
-	g.Run()
-	fmt.Println(g.GetGameDescribe())
+	e := echo.New()
+	e.GET("/start", startGame)
+	e.GET("/run", performRun)
+	e.GET("/stop", performRun)
+	e.Logger.Fatal(e.Start("127.0.0.1:1323"))
+}
+
+func startGame(c echo.Context) error {
+	ret := h.StartGame()
+	return c.String(http.StatusOK, ret)
+}
+
+func performRun(c echo.Context) error {
+	ret := h.Run()
+	return c.String(http.StatusOK, ret)
+}
+
+func stopGame(c echo.Context) error {
+	ret := h.FinishGame()
+	return c.String(http.StatusOK, ret)
 }
--- games/horse.go
+++ games/horse.go
@@ -2,13 +2,14 @@ package horse
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
 )
 
 // Game status structure
 type Game struct {
 	gameOn     bool
-	horses     map[string]*Horse
+	horses     map[int]*Horse
 	finishLine int
 }
 
@@ -29,14 +30,15 @@ func (g *Game) StartGame() string {
 		g.initGame()
 		return "200"
 	}
-	return "500"
+	return "游戏已经开始"
 }
 
 func (g *Game) initGame() {
 	g.finishLine = 1000
-	g.horses = make(map[string]*Horse)
-	g.horses["小花"] = newHorse()
-	g.horses["小明"] = newHorse()
+	g.horses = make(map[int]*Horse)
+	for i := 1; i < 5; i++ {
+		g.horses[i] = newHorse()
+	}
 }
 
 func newHorse() *Horse {
@@ -47,31 +49,78 @@ func newHorse() *Horse {
 	return &h
 }
 
+func getRandom(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
 // GetGameDescribe will return the literal describtion of the current game
 func (g *Game) GetGameDescribe() string {
-	a := ""
-	for k, v := range g.horses {
-		a += strings.Repeat("=", 20)
-		if v.alive {
-			// a += fmt.Sprintf("\n%v 当前位置：%v  状态：活着  BUFF：%.1f\n", k, v.position, v.buff)
-			a += fmt.Sprintf("\n" + strings.Repeat(" ", 19) + "🐎\n")
-		} else {
-			a += fmt.Sprintf("\n%v 当前位置：%v  状态：死了  BUFF：%.1f\n", k, v.position, v.buff)
+	laneLength := 36
+	sideLength := 20
+	minPosition := 1000
+	for _, v := range g.horses {
+		if v.position < minPosition {
+			minPosition = v.position
 		}
 	}
-	a += strings.Repeat("=", 20)
+	a := ""
+	for k, v := range g.horses {
+		if k != 1 {
+			a += strings.Repeat("=", sideLength)
+		}
+		leadRange := (v.position - minPosition)
+		if leadRange >= laneLength {
+			leadRange = laneLength
+		}
+		leadRange = laneLength - leadRange
+		if v.alive {
+			// a += fmt.Sprintf("\n%v 当前位置：%v  状态：活着  BUFF：%.1f\n", k, v.position, v.buff)
+			a += fmt.Sprintf("\n" + strings.Repeat(" ", leadRange) + "🐴\n")
+		} else {
+			a += fmt.Sprintf("\n" + strings.Repeat(" ", leadRange) + "☠️\n")
+		}
+	}
+	// a += strings.Repeat("=", 20)
+	a += "\n"
 	return fmt.Sprintf("%v", a)
 }
 
 // Run will apply random event on each horse, thus make the game continue
 // use the dispather to constantly call Run() until the game reaches end
 func (g *Game) Run() string {
-	g.horses["小花"].applyEvent()
-	g.horses["小明"].applyEvent()
-	return "1"
+	if g.gameOn {
+		event := ""
+		for i := range g.horses {
+			event += g.horses[i].applyEvent()
+			if g.horses[i].position > g.finishLine {
+				g.FinishGame()
+				event += fmt.Sprintf("\n游戏结束，%v号🐴获得了胜利\n", i)
+				// event += g.GetGameDescribe()
+				return event
+			}
+		}
+		event += "\n"
+		event += g.GetGameDescribe()
+		return event
+	}
+	return "游戏尚未开始"
+}
+
+// FinishGame finishes a current running game
+func (g *Game) FinishGame() string {
+	if g.gameOn {
+		g.gameOn = false
+		for i := 1; i < 6; i++ {
+			g.horses[i].position = 0
+			g.horses[i].alive = true
+			g.horses[i].buff = 0
+		}
+		return "游戏结束"
+	}
+	return "游戏尚未开始"
 }
 
 func (h *Horse) applyEvent() string {
-	h.position += 100
-	return "1"
+	h.position += getRandom(100, 120)
+	return ""
 }
